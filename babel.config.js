42eb1996580b003bf325ed15fdeae8da8e666f03
Tian Pan <puncsky@gmail.com>
2019-03-18 19:30:18 -0700

    add prettier & clean up

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,15 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', {
-      targets: {
-        node: 'current',
-      },
-    }],
-    ['@babel/preset-typescript'],
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current"
+        }
+      }
+    ],
+    ["@babel/preset-typescript"]
   ],
-  plugins: [
-    '@babel/plugin-proposal-object-rest-spread',
-    'transform-class-properties',
-  ],
-  ignore: [
-    /node_modules\//g,
-  ],
+  plugins: [],
+  ignore: [/node_modules\//g]
 };
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./dist/main.js');
+module.exports = require("./lib/index.js");
