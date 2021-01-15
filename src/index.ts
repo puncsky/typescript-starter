841c679b9533e48f1a7f49ac3f9d22203c9ebe4d
Tian Pan <puncsky@gmail.com>
2021-01-15 12:00:34 -0800

    update index.ts to run with npm run start

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ export async function delayedHello(
   name: string,
   delay = 2000
 ): Promise<string> {
+  const hello = `Hello, ${name}`;
+  console.log(hello);
   const task = (resolve: (result: string) => void) => {
     setTimeout(() => {
-      resolve(`Hello, ${name}`);
+      resolve(hello);
     }, delay);
   };
   return new Promise<string>(task);
